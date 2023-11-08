69adbaa232bd0eb9c77ebe1f72a1f035d08e0ab6
ramoj <ramoj@google.com>
2023-11-08 04:27:35 +0000

    Updates

--- grrshell/lib/grr_shell_client.py
+++ grrshell/lib/grr_shell_client.py
@@ -122,10 +122,6 @@ class GRRShellClient:
     self._last_seen_monitor = client_monitors.LastSeenMonitor(self._grr_stubby.Client(self._grr_client_id))
     self._last_seen_monitor.StartMonitor()
 
-  def Untested(self):
-    """Not covered by unit tests."""
-    print('This is not covered by unit tests')
-
   def WaitForBackgroundCompletions(self):
     """Destructor for GRRShellClient.
 

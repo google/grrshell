f460528c3c4699a02e6e3f1dda1b1992a2171edb
ramoj <ramoj@google.com>
2023-11-08 02:14:55 +0000

    Remove test code

--- grrshell/lib/grr_shell_client.py
+++ grrshell/lib/grr_shell_client.py
@@ -122,9 +122,6 @@ class GRRShellClient:
     self._last_seen_monitor = client_monitors.LastSeenMonitor(self._grr_stubby.Client(self._grr_client_id))
     self._last_seen_monitor.StartMonitor()
 
-  def untested(self):
-    print('This is not covered by unit tests')
-
   def WaitForBackgroundCompletions(self):
     """Destructor for GRRShellClient.
 
